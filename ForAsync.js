6cbff4ae365344f908e54c01e5637cf2d6a84491
picospuch <picospuch@gmail.com>
2016-07-23 15:24:05 +0800

    add async quit support

--- ForAsync.js
+++ ForAsync.js
@@ -1,13 +1,28 @@
 // for for event loop
 function ForAsync(init, check, final, body) {
   var self = this;
+  self.break = function () {
+    throw "forasync_break";
+  }
+  self.quit = function () {
+    self.brk = true;
+  }
+  self.brk = false;
   init.call(self);
   function loop(self) {
-    if (check.call(self)) {
-      body.call(self, function (t) {
-        final.call(self);
-        setTimeout(loop, Number(t), self);
-      });
+    if (check.call(self) && !self.brk) {
+      try {
+        body.call(self, function (t) {
+          final.call(self);
+          setTimeout(loop, Number(t), self);
+        });
+      } catch (e) {
+        if (e === "forasync_break") {
+          self.brk = true;
+        } else {
+          throw e;
+        }
+      }
     } else {
       ;
     }
