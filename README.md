196eec0cfe5c52a27a666c2ddaacf122d6045204
picospuch <picospuch@gmail.com>
2016-07-28 17:01:33 +0800

    prepare for sharing

---
+++ README.md
@@ -0,0 +1,42 @@
+# Async For Loop Implement
+
+a for loop for async functions.
+
+```js
+var fora = require('fora');
+
+fora(function () {
+  this.i = 0;
+}, function () {
+  return this.i < 10;
+}, function () {
+  ++this.i;
+}, function (done) {
+  setTimeout(() => {
+    console.log(this.i);
+    done();
+  }, 1000);
+});
+```
+⬆this mimics this⬇
+```js
+for (
+// function () {
+  var i = 0;
+// }, function () {
+  i < 10;
+// }, function () {
+  ++i
+) {
+  console.log(i);
+}
+```
+
+## Installation
+
+npm install async-for
+
+## License
+
+MIT
+
--- test.js
+++ test.js
@@ -1,5 +1,32 @@
-var fora = require('./ForAsync');
+var fora = require('./fora');
 
+function basic() {
+fora(function () {
+  this.i = 0;
+}, function () {
+  return this.i < 10;
+}, function () {
+  ++this.i;
+}, function (done) {
+  setTimeout(() => {
+    console.log(this.i);
+    done();
+  }, 1000);
+});
+
+for (
+// function () {
+  var i = 0;
+// }, function () {
+  i < 10;
+// }, function () {
+  ++i
+) {
+  console.log(i);
+}
+} //~basic
+
+function normal() {
 var f = fora(function () {
   this.i = 0;
 }, function () {
@@ -10,4 +37,24 @@ var f = fora(function () {
   console.log(this.i);
   done();
 });
+} //~normal
 
+function quit() {
+var f = fora(function () {
+  this.i = 0;
+}, function () {
+  return this.i < 10;
+}, function () {
+  this.i++;
+}, function (done) {
+  console.log(this.i);
+  if (this.i == 5) {
+    setTimeout(() => {
+      this.quit();
+    }, 0);
+  }
+  done();
+});
+}
+
+basic();
